e2d993044bb13e57a0b82f92675f8a00e80a1af6
Luis Castillo <luisc58@Luiss-MacBook-Pro.local>
2023-10-21 17:12:20 -0400

    analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,10 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({ applyBaseStyles: false }), react()],
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
